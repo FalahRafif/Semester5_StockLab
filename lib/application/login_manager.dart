a556217993b982c4c0a538d175c359739a9233a2
falahrafif <frthegamer95@gmail.com>
2025-12-07 15:38:12 +0700

    add color manager and change respone login

--- lib/application/login_manager.dart
+++ lib/application/login_manager.dart
@@ -1,26 +1,22 @@
 import '../data/repositories/login_repository.dart';
+import '../data/models/login_response.dart';
 
 class LoginManager {
   final repo = LoginRepository();
 
-  Future<Map<String, dynamic>> login({
+  Future<LoginResponse> login({
     required String email,
     required String password,
   }) async {
     if (email.isEmpty || password.isEmpty) {
-      return {
-        "success": false,
-        "message": "Email dan password tidak boleh kosong"
-      };
+      return LoginResponse(
+        success: false,
+        message: "Email dan password tidak boleh kosong",
+      );
     }
 
     final result = await repo.login(email, password);
-
-    return {
-      "success": result.success,
-      "message": result.message,
-      "token": result.token,
-      "user_id": result.userId,
-    };
+    print(result);
+    return result;
   }
 }
